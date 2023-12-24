1b912d325ea72d0877bdfbcc75c8644d71f3f93f
Otavio ACdeC <gutsgt1@gmail.com>
2023-12-24 15:07:02 -0300

    save

--- first_push.bat
+++ first_push.bat
@@ -1,5 +1,5 @@
 cd %appdata%\.minecraft\saves\Home_Sweet_Home
 git init
-git remote add home_sweet_home https://github.com/GutsGT/Home-Sweet-Home.git
+git remote add home_sweet_home https://github.com/GutsGT/Home_Sweet_Home.git
 git branch -M main
 push
--- push.bat
+++ push.bat
@@ -2,4 +2,4 @@ cd %appdata%\.minecraft\saves\Home_Sweet_Home
 git add .
 git commit -m "save"
 git push home_sweet_home main
-start https://github.com/GutsGT/Home-Sweet-Home
+start https://github.com/GutsGT/Home_Sweet_Home
